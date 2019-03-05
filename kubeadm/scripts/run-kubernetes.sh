cb5e9619f28b0c85dd1d8a2e8fd1777dacbda546
Jgutierrez <juandavidgc@gmail.com>
2019-03-05 11:02:32 -0500

    Se adicionan archivos para instalación y ejecución de kubeadm

---
+++ kubeadm/scripts/run-kubernetes.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "deploying kubernetes (with canal)..."
+kubeadm init --pod-network-cidr=172.16.0.0/16 # add --apiserver-advertise-address="ip" if you want to use a different IP address than the main server IP
+export KUBECONFIG=/etc/kubernetes/admin.conf
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/rbac.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/canal/canal.yaml
